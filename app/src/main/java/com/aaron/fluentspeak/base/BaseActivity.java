15e3d1f79a4596af4af225cccc274df86b5c21b5
Aaron <aaronhuang51@gmail.com>
2015-03-27 22:00:47 +0800

    create

---
+++ app/src/main/java/com/aaron/fluentspeak/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.aaron.fluentspeak.base;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ *
+ * Activity基类
+ * Created on 下午9:49.
+ *
+ * @author ran.huang
+ * @version 1.0.0
+ */
+public class BaseActivity extends FragmentActivity {
+}
